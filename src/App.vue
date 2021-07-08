0a2cfa97882a8813980b1790f90a29569e91d0f2
acharayaP03 <acharyaP03@gmail.com>
2021-07-08 13:37:34 +1000

    typescript with option api

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
-<template> 
-<h1>Hello Type script</h1>
+<template>
+  <h1>Hello {{ name }} - {{ age }}</h1>
+  <button @click="changeName('Trishten Ac ')">change name</button>
+  <button @click="changeAge(39)">Change age</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'App',
-  components: {
-  }
-});
+  components: {},
+  data() {
+    return {
+      name: 'link',
+      age: 38 as number | string,
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      return (this.name = name)
+    },
+    changeAge(age: number | string) {
+      return (this.age = age)
+    },
+  },
+})
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap');
-body{
-    font-family: 'Open Sans';
-    background-color: #ece6d9;
+body {
+  font-family: 'Open Sans';
+  background-color: #ece6d9;
 }
-h1{
+h1 {
   margin: 0 auto;
   text-align: center;
 }
