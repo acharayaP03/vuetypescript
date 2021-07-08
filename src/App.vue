fd90e9912907de45e951350c947b90bf5339d10f
acharayaP03 <acharyaP03@gmail.com>
2021-07-08 11:15:58 +1000

    Intitial project setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template> 
+<h1>Hello Type script</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  }
+});
+</script>
+
+<style>
+h1{
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
